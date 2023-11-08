70a4f8d03d9c7f4bdf1c5d941a13d790da4c6216
Dhinesh4668 <dhinesh4668@gmail.com>
2023-11-08 15:23:45 +0530

    github action setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Game from "./src/components/Game";
+import { SafeAreaView } from "react-native";
 
 const App = (): JSX.Element => (
-  <GestureHandlerRootView style={{ flex: 1 }}>
-    <Game />
-  </GestureHandlerRootView>
+  <SafeAreaView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Game />
+    </GestureHandlerRootView>
+  </SafeAreaView>
 );
 
 export default App;
